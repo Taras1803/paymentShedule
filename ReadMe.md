a7ff48c5c01bc2000ca3bd6597417d01572b8dce
Gilbert <gilbert@gethooked.nl>
2022-08-23 23:55:57 +0200

    refactor

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,16 @@
-Use Carbon, for this task it is very good solution
+Use **Carbon**, for this task it is very good solution
 
-also symphony dumper for developing process (not necessary)
+also **symphony dumper** for developing process (not necessary)
+
+The logic propose to way of usages:
+
+1.Without date, starting generate csv from today.
+
+2.And with argument of specific date, starting generate csv from argument passed date.
+
+**EXAMPLES USAGES**
 
 console run: 'php index.php'
+console run: 'php index.php 01-01-2022'
+console run: 'php index.php 31-03-2022'
 
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require 'vendor/autoload.php';
 
 use Carbon\Carbon;
+use Carbon\Exceptions\InvalidFormatException;
 
 const WEEKEND = [Carbon::SATURDAY, Carbon::SUNDAY];
 
@@ -16,25 +17,39 @@ function getPaymentDay($day)
     return in_array($day->dayOfWeek, WEEKEND) ? $day->previous(Carbon::FRIDAY) : $day;
 }
 
-$data = [];
-$today = Carbon::now();
 
+
+
+$data = [];
+
+$today = Carbon::now();
+$endOfMonth = Carbon::now()->endOfMonth();
+if(isset($argv[1])){
+    try {
+        Carbon::parse($argv[1]);
+        $today = Carbon::createFromFormat('d-m-Y', $argv[1]);
+        $endOfMonth = Carbon::createFromFormat('d-m-Y', $argv[1])->endOfMonth();
+    } catch (InvalidFormatException $e) {
+        echo 'invalid date format';
+        die();
+    }
+}
 
 // Current month logic
 $bonusDay = Carbon::parse($today->year . '-' . $today->month . '-15');
 $data[] = [
     'month' => $today->format('F'),
-    'payment' => in_array($today->dayOfWeek, WEEKEND) && getPaymentDay($today->endOfMonth())->isPast() ? '-' : getPaymentDay($today->endOfMonth())->format('d-m-Y'),
+    'payment' => in_array($today->dayOfWeek, WEEKEND) && getPaymentDay($endOfMonth) < $today ? '-' : getPaymentDay($endOfMonth)->format('d-m-Y'),
     'bonus' => $today->day <= 15 ? getBonusDay($bonusDay)->format('d-m-Y') : '-'
 ];
-
 // Not current month logic
 for ($i = $today->month + 1; $i <= 12; $i++) {
     $startOfMonth = Carbon::parse($today->year . '-' . $i . '-01');
     $bonusDay = Carbon::parse($today->year . '-' . $i . '-15');
+    $endOfMonth = Carbon::parse($today->year . '-' . $i . '-01')->endOfMonth();
     $data[] = [
         'month' => $startOfMonth->format('F'),
-        'payment' => getPaymentDay($startOfMonth->endOfMonth())->format('d-m-Y'),
+        'payment' => getPaymentDay($endOfMonth)->format('d-m-Y'),
         'bonus' => getBonusDay($bonusDay)->format('d-m-Y')
     ];
 }
